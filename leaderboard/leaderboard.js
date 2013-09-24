e598fb14669bce8d76bd9a8ad5072a51c5089bf2
thebrettd <thebrettd@gmail.com>
2013-09-24 14:10:08 -0700

    Working randomize Formatting

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -5,44 +5,44 @@ Players = new Meteor.Collection("players");
 
 if (Meteor.isClient) {
 
-  Session.set("mySort","score");
+    Session.set("mySort", "score");
 
-  Template.leaderboard.players = function () {
-      if (Session.equals("mySort","score")){
-          return Players.find({}, {sort: {score: -1, name: 1}});
-      }else{
-          return Players.find({}, {sort: {name: 1, score: 1}});
-      }
-  };
+    Template.leaderboard.players = function () {
+        if (Session.equals("mySort", "score")) {
+            return Players.find({}, {sort: {score: -1, name: 1}});
+        } else {
+            return Players.find({}, {sort: {name: 1, score: 1}});
+        }
+    };
 
-  Template.leaderboard.selected_name = function () {
-    var player = Players.findOne(Session.get("selected_player"));
-    return player && player.name;
-  };
+    Template.leaderboard.selected_name = function () {
+        var player = Players.findOne(Session.get("selected_player"));
+        return player && player.name;
+    };
 
-  Template.player.selected = function () {
-    return Session.equals("selected_player", this._id) ? "selected" : '';
-  };
+    Template.player.selected = function () {
+        return Session.equals("selected_player", this._id) ? "selected" : '';
+    };
 
-  Template.leaderboard.events({
-    'click input.inc': function () {
-      Players.update(Session.get("selected_player"), {$inc: {score: 5}});
-    }
-  });
+    Template.leaderboard.events({
+        'click input.inc': function () {
+            Players.update(Session.get("selected_player"), {$inc: {score: 5}});
+        }
+    });
 
-  Template.player.events({
-    'click': function () {
-      Session.set("selected_player", this._id);
-    }
-  });
+    Template.player.events({
+        'click': function () {
+            Session.set("selected_player", this._id);
+        }
+    });
 
     Template.leaderboard.events({
         'click input.sortToggle': function () {
             currSort = Session.get("mySort");
-            if(currSort == "name"){
+            if (currSort == "name") {
                 Session.set("mySort", "score");
-            }else{
-                Session.set("mySort","name");
+            } else {
+                Session.set("mySort", "name");
             }
         }
     });
@@ -56,25 +56,27 @@ if (Meteor.isClient) {
 
 // On server startup, create some players if the database is empty.
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    if (Players.find().count() === 0) {
-      var names = ["Ada Lovelace",
-                   "Grace Hopper",
-                   "Marie Curie",
-                   "Carl Friedrich Gauss",
-                   "Nikola Tesla",
-                   "Claude Shannon"];
+    Meteor.startup(function () {
+        if (Players.find().count() === 0) {
+            var names = ["Ada Lovelace",
+                "Grace Hopper",
+                "Marie Curie",
+                "Carl Friedrich Gauss",
+                "Nikola Tesla",
+                "Claude Shannon"];
 
-      for (var i = 0; i < names.length; i++)
-          Players.insert({name: names[i]});
-    }
-      Meteor.call("randomizeScore");
-  });
+            for (var i = 0; i < names.length; i++)
+                Players.insert({name: names[i]});
+        }
+        Meteor.call("randomizeScore");
+    });
 
-  Meteor.methods({
+    Meteor.methods({
         randomizeScore: function () {
-            //todo: foreach here to generate more than 1 random number
-            Players.update({},{$set: {score: Math.floor(Random.fraction()*10)*5}},{multi: true});
+            var players = Players.find({});
+            players.forEach(function (player) {
+                Players.update({name: player.name}, {$set: {score: Math.floor(Random.fraction() * 10) * 5}});
+            });
         }
     });
 }
